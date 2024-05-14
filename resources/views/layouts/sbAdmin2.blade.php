99051cc59c985fb5efd2a0f23278eb5760d550a0
typingmonk <tmonk.hsieh@tmonk.tw>
2024-05-14 14:58:46 +0800

    Change icon on sidebar

--- resources/views/layouts/sbAdmin2.blade.php
+++ resources/views/layouts/sbAdmin2.blade.php
@@ -24,8 +24,8 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+                <div class="sidebar-brand-icon">
+                    <i class="fas fa-landmark"></i>
                 </div>
                 <div class="sidebar-brand-text mx-3">立院統合資料網 Dataly</div>
             </a>

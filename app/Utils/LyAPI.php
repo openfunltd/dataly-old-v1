d1c7681ff1fd695298b8612c870ce32261538190
typingmonk <tmonk.hsieh@tmonk.tw>
2024-05-14 14:12:34 +0800

    Add function paginationRequestion to /App/Utils/LyAPI #10

--- app/Utils/LyAPI.php
+++ app/Utils/LyAPI.php
@@ -2,10 +2,28 @@
 
 namespace App\Utils;
 
+use Illuminate\Support\Facades\Http;
+
 class LyAPI
 {
-    public static function paginationRequest($url)
+    public static function paginationRequest($url, $target)
     {
-        //
+        $res = Http::get($url);
+        if (! $res->successful()) {
+            return [];
+        }
+        $res_json = $res->json();
+        $data = $res_json[$target];
+        $pages = $res_json['total_page'];
+        if ($pages < 2) {
+            return $data;
+        }
+        for ($i = 2; $i <= $pages; $i++) {
+            $res = Http::get($url . "?page=$i");
+            if ($res->successful()) {
+                $data = array_merge($data, $res->json()[$target]);
+            }
+        }
+        return $data;
     }
 }

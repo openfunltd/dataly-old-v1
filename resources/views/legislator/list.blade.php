bbab85279550ca3da65fd8fa94ad212e53f60a45
typingmonk <tmonk.hsieh@tmonk.tw>
2024-05-14 14:13:46 +0800

    Display legislator data in dataTables #9

--- resources/views/legislator/list.blade.php
+++ resources/views/legislator/list.blade.php
@@ -11,18 +11,75 @@
     <!-- DataTales -->
     <div class="card shadow mb-4">
       <div class="card-header py-1">
+        <span>屆期：</span>
+        @foreach ($terms as $term)
+        <a href="{{ route('legislators', ['term' => $term])}}"
+          class="btn {{ ($term == $params['term']) ? 'btn-danger' : 'btn-info'}} btn-sm"
+        >
+          <span class="text">{{ __('第 :term 屆', ['term' => $term])}}</span>
+        </a>
+        @endforeach
+      </div>
+      <div class="card-header py-1">
+        <span>會期：</span>
+        @foreach ($sessionPeriods as $sessionPeriod)
+            <a href="{{ route('legislators', [
+                'term' => $params['term'],
+                'sessionPeriod' => $sessionPeriod,
+            ])}}"
+            class="btn {{ ($sessionPeriod == $params['sessionPeriod']) ? 'btn-danger' : 'btn-info'}}
+                btn-sm">
+                @if ($sessionPeriod == 'all')
+                    <span class="text">{{ __('不篩選')}}</span>
+                @else
+                    <span class="text">{{ __('第 :sp 會期', ['sp' => $sessionPeriod])}}</span>
+                @endif
+            </a>
+        @endforeach
       </div>
       <div class="card-body">
         <div class="table-responsive" style="overflow-x: auto;">
           <table class="table table-bordered table-hover table-sm nowrap" id="dataTable" width="100%" cellspacing="0">
             <thead>
               <tr>
+                  <th class="dt-head-center">@lang('姓名')</th>
+                  <th class="dt-head-center">@lang('政黨')</th>
+                  <th class="dt-head-center">@lang('所屬委員會')</th>
+                  <th class="dt-head-center">@lang('選區')</th>
               </tr>
             </thead>
             <tbody>
+                @foreach ($legislators as $legislator)
+                    <tr>
+                        {{-- 姓名 --}}
+                        <td>{{ $legislator['name'] }}</td>
+                        {{-- 政黨 --}}
+                        <td>{{ $legislator['party'] }}</td>
+                        {{-- 所屬委員會 --}}
+                        <td>
+                            <?php $keys = array_keys($legislator['committee']); ?>
+                            @foreach ($legislator['committee'] as $key => $comt)
+                                @if ($params['term'] < 4 || $params['sessionPeriod'] == 'all')
+                                    {{ $comt }}
+                                @else
+                                    {{ explode('：', $comt)[1] }}
+                                @endif
+                                @if ($key != end($keys))
+                                    <br>
+                                @endif
+                            @endforeach
+                        </td>
+                        {{-- 選區 --}}
+                        <td>{{ $legislator['areaName'] }}</td>
+                    </tr>
+                @endforeach
             </tbody>
             <tfoot>
               <tr>
+                  <th>姓名</th>
+                  <th>政黨</th>
+                  <th>所屬委員會</th>
+                  <th>選區</th>
               </tr>
             </tfoot>
           </table>

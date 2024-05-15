fa6d0002c50f0fc906164472b51bea27e56a6d38
typingmonk <tmonk.hsieh@tmonk.tw>
2024-05-15 08:43:31 +0800

    Add party color code #11

---
+++ app/Utils/Helper.php
@@ -0,0 +1,14 @@
+
+<?php
+
+namespace App\Utils;
+
+class Helper
+{
+    // Color code on https://zh.wikipedia.org/zh-tw/Template:中華民國政黨色彩
+    public static $party_colors = [
+        '民主進步黨' => '#1B9431',
+        '國民黨' => '#000095',
+        '台灣民眾黨' => '#28C8C8',
+    ];
+}

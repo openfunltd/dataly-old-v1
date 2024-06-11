0d38f446bbc77315f6fb0deb300cc9ff5907f44f
typingmonk <tmonk.hsieh@tmonk.tw>
2024-06-11 10:00:05 +0800

    Executing text_diff_worker.js and get result back to php in app/Util/TextDiff.php #14

---
+++ app/Utils/TextDiff.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Utils;
+
+class TextDiff
+{
+    public static function prettyHtmls($contents)
+    {
+        $input = tempnam('/tmp/', 'law-diff-');
+        file_put_contents($input, json_encode($contents));
+        $output = tempnam('/tmp/', 'law-diff-');
+
+        $cmd = sprintf("env NODE_PATH=%s node %s %s %s",
+            escapeshellarg(base_path() . '/node_modules'),
+            escapeshellarg(base_path() . '/scripts/text_diff_worker.js'),
+            escapeshellarg($input),
+            escapeshellarg($output)
+        );
+
+        system($cmd, $ret);
+
+        $result = file_get_contents($output);
+        $result = json_decode($result);
+
+        unlink($input);
+        unlink($output);
+        //dd($contents);
+        //dd(json_encode($contents));
+        dd($result, $ret);
+        return $result;
+    } 
+}

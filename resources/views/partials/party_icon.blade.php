4bab02c3dbc646b143f51c1874f91e3588653a57
typingmonk <tmonk.hsieh@tmonk.tw>
2024-06-21 11:00:08 +0800

    Add title attribute to party_icon <img> #17

--- resources/views/partials/party_icon.blade.php
+++ resources/views/partials/party_icon.blade.php
@@ -1,4 +1,4 @@
 <?php $party_icon_url = App\Utils\LegislatorHelper::$party_icon_url; ?>
 @if (array_key_exists($party, $party_icon_url))
-    <img class="party-icon" src="{{ asset($party_icon_url[$party]) }}">
+    <img class="party-icon" src="{{ asset($party_icon_url[$party]) }}" title="{{ $party }}">
 @endif

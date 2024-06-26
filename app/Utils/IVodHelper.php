4151921e1e2b102c0631705f80f6acc7e18999a2
typingmonk <tmonk.hsieh@tmonk.tw>
2024-06-26 09:32:33 +0800

    Temp Fix: Support chinses index up to 29

--- app/Utils/IVodHelper.php
+++ app/Utils/IVodHelper.php
@@ -6,7 +6,8 @@ class IVodHelper
 {
     public static function getSubjects($meet_name) {
         $first_order_indexes = ['一、', '二、', '三、', '四、', '五、', '六、', '七、', '八、', '九、', '十、',
-            '十一、', '十二、', '十三、', '十四、', '十五、', '十六、', '十七、', '十八、', '十九、', '二十、'];
+            '十一、', '十二、', '十三、', '十四、', '十五、', '十六、', '十七、', '十八、', '十九、', '二十、',
+            '二十一', '二十二', '二十三', '二十四', '二十五', '二十六', '二十七', '二十八', '二十九', '三十'];
         $content = self::parseReason(trim($meet_name));
         $with_first_order_index = mb_strpos($content, $first_order_indexes[0]) === 0;
 

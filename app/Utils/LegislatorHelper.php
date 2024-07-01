e7237e4ac19a837705cdac4c7491e28932bd5505
typingmonk <tmonk.hsieh@tmonk.tw>
2024-07-01 18:12:41 +0800

    Get legislators bioId in IvodController #17

--- app/Utils/LegislatorHelper.php
+++ app/Utils/LegislatorHelper.php
@@ -13,10 +13,17 @@ class LegislatorHelper
         '無黨籍' => 'img/party_icon/none.png',
     ];
 
-    public static function requestLegislatorPartyMap($term)
+    public static function requestLegislators($term)
     {
         $res = LyAPI::apiQuery('/legislator/11?limit=300', '查詢立法委員所屬政黨');
-        $legislators = $res->legislators;
+        return $res->legislators;
+    }
+
+    //TODO retire the function
+    public static function requestLegislatorPartyMap($term)
+    {
+        $res = LyAPI::apiQuery("/legislator/$term?limit=300", '查詢立法委員所屬政黨');
+        return $res->legislators;
         $legislator_party_map = [];
         foreach ($legislators as $legislator) {
             $name = str_replace(" ", "", $legislator->name);
@@ -24,5 +31,21 @@ class LegislatorHelper
             $legislator_party_map[$name] = $legislator->party;
         }
         return $legislator_party_map;
+    }
+
+    public static function getLegislatorsBasicInfo($legislators)
+    {
+        $legislators_basic_info = new \stdClass();
+        foreach ($legislators as $legislator) {
+            $name = str_replace(" ", "", $legislator->name);
+            $name = str_replace("‧", "", $name);
+            $party = $legislator->party;
+            $bio_id = $legislator->bioId;
+            $legislators_basic_info->{$name} = (object) [
+                'party' => $party,
+                'bio_id' => $bio_id,
+            ];
+        }
+        return $legislators_basic_info;
     }
 }
